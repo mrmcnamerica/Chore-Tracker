6ba4ab3fedde76923aa3eea538a10dcdfd334d6f
Lionel McNamara <lionel.mcnamara@icloud.com>
2025-10-11 14:07:15 -0400

    Fix PWA Authentication

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -3,4 +3,11 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = 'https://dihwovhytzrxubfgseai.supabase.co'
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRpaHdvdmh5dHpyeHViZmdzZWFpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjAxNTE0NjgsImV4cCI6MjA3NTcyNzQ2OH0.agEN715-zWnqRlp0aDCq5e0M6pubLmbVquZExlqFIzM'
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    flowType: 'pkce',
+    detectSessionInUrl: true,
+    persistSession: true,
+    autoRefreshToken: true
+  }
+})
